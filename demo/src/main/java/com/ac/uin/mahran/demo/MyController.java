06a6ce502af301caae438ea3acf924acac05f9f0
Mahran24 <83098859+Mahran24-arch@users.noreply.github.com>
2022-03-22 16:00:28 +0700

    Add files via upload

---
+++ demo/src/main/java/com/ac/uin/mahran/demo/MyController.java
@@ -0,0 +1,13 @@
+package com.ac.uin.mahran.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+
+public class MyController {
+    @GetMapping ("/hello")
+    public String hello() {return "Selamat Belajar Spring Boot !";}
+}
